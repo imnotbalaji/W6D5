ef5cea0b795d78a784e58e2bf79f4258ea6e8e2e
Balaji <imnotbalaji@gmail.com>
2023-09-29 15:08:02 -0400

    completing first migration with some model changes

--- ninety-nine-cats/app/models/cat.rb
+++ ninety-nine-cats/app/models/cat.rb
@@ -1,2 +1,6 @@
 class Cat < ApplicationRecord
+    validates :name, presence: true
+    validates :color, presence: true, 
+    validates :birth_date, presence: true
+    validates :sex, presence: true
 end

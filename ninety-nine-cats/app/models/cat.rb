3c31eb7e7f8af0a687878fb4139f9f7225d012c5
omar-t-ahmed <omartahmed@gmail.com>
2023-09-29 16:03:29 -0400

    complete cat methods

--- ninety-nine-cats/app/models/cat.rb
+++ ninety-nine-cats/app/models/cat.rb
@@ -1,6 +1,20 @@
 class Cat < ApplicationRecord
+    CAT_COLORS = ['black', 'orange', 'white', 'brown']
+
     validates :name, presence: true
-    validates :color, presence: true, 
+    validates :color, presence: true, inclusion: { in: CAT_COLORS, message: 'cat color not valid'}
     validates :birth_date, presence: true
-    validates :sex, presence: true
+    validates :sex, presence: true, inclusion: {in: ['M', 'F'], message: 'sex not valid'}
+    validate :birth_date_cannot_be_future
+
+    def self.birth_date_cannot_be_future
+        if (birth_date <=> Date.today) != 1
+            errors.add(:birth_date, 'birth date cant be in the future')
+        end
+    end
+
+    def age
+        Date.today.year - birth_date.year
+    end
+
 end
